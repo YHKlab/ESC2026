530731e7082863d8f7a44f19fdc6f75170d0ddeb
Ryong-Gyu <dlfydrb@gmail.com>
2026-07-10 18:03:28 +0900

    fix input configuration

--- exe2-siesta/0.preliminary/setup.sh
+++ exe2-siesta/0.preliminary/setup.sh
@@ -12,4 +12,19 @@ echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 grep -qxF "export PATH=\"$(pwd)/scripts:\${PATH}\"" ~/.bashrc || \
 echo "export PATH=\"$(pwd)/scripts:\${PATH}\"" >> ~/.bashrc
 
-source ~/.bashrc
+cd ..
+cat > ./py4siesta/NanoCore/env.py <<'EOF'
+#
+# SIESTA
+#
+
+siesta_calculator = '/opt/siesta-5.4.2-install/bin/siesta'
+siesta_psf_location = ''
+siesta_util_location = '/opt/siesta-5.4.2-install/bin/'
+siesta_util_band = 'gnubands'
+siesta_util_dos  = 'Eig2DOS'
+siesta_util_pdos = 'fmpdos'
+siesta_util_rho  = 'rho2xsf'
+siesta_util_vh   = 'macroave'
+siesta_util_pldos = ''
+EOF
