4336616a706fae192390fb2f2d80cc36d39ea9af
YHKLab-RGLee <ronggyulee@kiast.ac.kr>
2026-07-12 17:08:18 +0900

    Fix setup.sh

--- exe2-siesta/0.preliminary/setup.sh
+++ exe2-siesta/0.preliminary/setup.sh
@@ -4,16 +4,7 @@ set -e
 git clone https://github.com/YHKlab-RGLee/py4siesta.git
 cd py4siesta
 
-python -m pip install --user .
-
-grep -qxF 'export PATH="$HOME/.local/bin:$PATH"' ~/.bashrc || \
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-
-grep -qxF "export PATH=\"$(pwd)/scripts:\${PATH}\"" ~/.bashrc || \
-echo "export PATH=\"$(pwd)/scripts:\${PATH}\"" >> ~/.bashrc
-
-cd ..
-cat > ./py4siesta/NanoCore/env.py <<'EOF'
+cat > ./NanoCore/env.py <<'EOF'
 #
 # SIESTA
 #
@@ -28,3 +19,20 @@ siesta_util_rho  = 'rho2xsf'
 siesta_util_vh   = 'macroave'
 siesta_util_pldos = ''
 EOF
+
+INSTALL_PREFIX="$HOME/opt/py4siesta"
+mkdir -p "$INSTALL_PREFIX"
+
+python -m pip install --prefix "$INSTALL_PREFIX" .
+
+SITE_PACKAGES=$(find "$INSTALL_PREFIX/lib" \
+    -type d -path '*/site-packages' | head -n 1)
+
+grep -qxF "export PATH=\"$(pwd)/scripts:\${PATH}\"" ~/.bashrc || \
+    echo "export PATH=\"$(pwd)/scripts:\${PATH}\"" >> ~/.bashrc
+
+grep -qxF 'export PATH="$HOME/opt/py4siesta/bin:$PATH"' ~/.bashrc || \
+    echo 'export PATH="$HOME/opt/py4siesta/bin:$PATH"' >> ~/.bashrc
+
+grep -qxF "export PYTHONPATH=\"$SITE_PACKAGES:\${PYTHONPATH:-}\"" ~/.bashrc || \
+    echo "export PYTHONPATH=\"$SITE_PACKAGES:\${PYTHONPATH:-}\"" >> ~/.bashrc
